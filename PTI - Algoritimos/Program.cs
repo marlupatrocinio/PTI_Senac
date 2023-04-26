fe33387005f440cb263a012a401964ca23bc5790
Marlu Patrocínio <107137108+marlupatrocinio@users.noreply.github.com>
2023-04-26 11:11:08 -0300

    First Commit

---
+++ PTI - Algoritimos/Program.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        bool resposta = true;
+        while (resposta)
+        {
+            Console.Write("Qual é o valor da gasolina?");
+            double gasolina = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Qual é o valor do álcool?");
+            double alcool = Convert.ToDouble(Console.ReadLine());
+
+            double parametro = 0.7;
+            double resultado = alcool/gasolina;
+
+            if (resultado > parametro) {
+                Console.WriteLine("Gasolina é mais vantajoso");
+            }
+            else if (resultado < parametro) {
+                Console.WriteLine("Alcool é mais vantajoso");
+            }
+            else {
+                Console.WriteLine("Abasteça com qualquer uma das opções");
+            }
+            Console.WriteLine("Deseja calcular novamente?");
+            string x = Console.ReadLine();
+            if (x.ToUpper() != "S") {
+                resposta = false;
+            }
+        }
+    }
+}
